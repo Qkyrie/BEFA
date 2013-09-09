361a83c0f630dd5fa49f68826acde909a534c80b
Quinten De Swaef <quintendeswaef@gmail.com>
2013-09-09 21:03:51 +0200

    created sdk, created a maven project from it. Didnt publish it yet, to protect our sources.
    Added it as extra library, together with the precise sdk.
    
    removed abe sdk, was empty

---
+++ SDK/gen/com/trust1t/befa/sdk/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.trust1t.befa.sdk;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SDK/target/generated-sources/r/com/trust1t/befa/sdk/BuildConfig.java
@@ -0,0 +1,5 @@
+package com.trust1t.befa.sdk;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = true;
+}
